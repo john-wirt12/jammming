88eaa2654226bc1b458235e79b4d548ae2118fb8
Your Name <you@example.com>
2025-01-29 02:16:19 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Spotify API Project</h1>
+    </>
+  )
+}
+
+export default App
